173a73b011097df63a1a284755fb3dd7a45c4512
DN050498MAM <andrii.moskalenko@privatbank.ua>
2023-01-12 19:50:26 +0200

    Fix endpoint

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ app.use(express.json());
 
 app.use('/auth', discordAuth);
 
+let redirectURL = 'https://discord.com/api/oauth2/authorize?client_id=1063133464741818390&redirect_uri=https%3A%2F%2Fmountain-marbled-diagnostic.glitch.me%2Fauth%2Fprofile&response_type=code&scope=identify';
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-})
+    res.send(`<h1>Hello World</h1><a href="${redirectURL}">Login</a>`);
+});
 
 module.exports = app;
--- discord-oauth2.js
+++ discord-oauth2.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/profile', (req, res) => {
-    const body = req.body;
-    res.json(body);
+    const {code} = req.query;
+    res.cookie('code', code, {path: "/", maxAge: '6000000'});
+    res.send('<h1>Authorized</h1>');
 });
 
 
-module.exports = router;
+
+
+module.exports = router;        
